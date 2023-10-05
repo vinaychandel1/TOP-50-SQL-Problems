86d5e2cb0e550c4384b8472b5704f753820a3783
VIINNAY CHANDEL <38590400+vinaychandel1@users.noreply.github.com>
2023-10-05 20:10:48 +0530

    Create 584. Find Customer Referee.sql

---
+++ 584. Find Customer Referee.sql
@@ -0,0 +1,4 @@
+# Write your MySQL query statement below
+select name from customer where referee_id<>2 OR referee_id is null;
+
+# IS keyword in SQL is primarily used to work with NULL values and boolean expressions
